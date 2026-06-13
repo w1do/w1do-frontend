e9a8fef442935d1a172e910a5f46d8bc25fb2302
UNIQDEVELOPER <uniqdeveloper@yandex.ru>
2026-06-13 18:58:05 +0500

    fix form

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://w1do.ru',
-  output: 'server',
+  output: 'static',
   adapter: node({
     mode: 'standalone'
   }),
